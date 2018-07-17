9f07dcf0476295e82595dc4ccd4cb67468ce3256
prasad.srg <prasad.srg@gmail.com>
2018-07-17 14:46:48 +0530

    commit

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DashboardContainerComponent } from "./containers/dashboard-container/dashboard-container.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: DashboardContainerComponent },
+  { path: "dashboard", component: DashboardContainerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { MaterialModule } from "../../material.module";
+import { DashboardContainerComponent } from "./containers/dashboard-container/dashboard-container.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ],
-  declarations: []
+  imports: [CommonModule, DashboardRoutingModule, MaterialModule],
+  declarations: [DashboardComponent, DashboardContainerComponent]
 })
-export class DashboardModule { }
+export class DashboardModule {}
