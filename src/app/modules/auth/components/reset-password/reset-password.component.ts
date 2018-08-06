ee8599e797afb65ee579d7215d9bd08aac1616fd
sairambitra7 <sairambitra7@gmail.com>
2018-08-06 19:15:03 +0530

    reset component done

--- src/app/modules/auth/components/reset-password/reset-password.component.ts
+++ src/app/modules/auth/components/reset-password/reset-password.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from "../../../../entities/user";
+import { ControlContainer, NgForm } from "@angular/forms";
+import { Props } from "../../../../common/props";
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+  styleUrls: ['./reset-password.component.scss'],
+  viewProviders: [{ provide: ControlContainer, useExisting: NgForm }]
 })
 export class ResetPasswordComponent implements OnInit {
+@Input() auth: User;
+  showorhide: string;
+  isVisible: string;
+  error: any;
+  Props: any = Props;
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
-
+ 
+resetPassword(){
+  
+}
 }
--- src/app/modules/auth/containers/reset-password-container/reset-password.container.ts
+++ src/app/modules/auth/containers/reset-password-container/reset-password.container.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { User } from "../../../../entities/user";
 import { Props } from "../../../../common/props";
+import { AuthService } from "../../common/auth.service";
+import { Storage } from "../../../../shared/utils/storage";
 
 @Component({
   selector: "app-reset-password-container",
   templateUrl: "./reset-password.container.html"
 })
 export class ResetPasswordContainer implements OnInit {
+  auth: User;
   Props: any = Props;
-  @Input() auth: User;
-  constructor() {}
+  constructor(private authService: AuthService) {
+    Storage.clearSession();
+    this.auth = new User();
+    // ResetPasswordForm.edit(this.myForm);
+    // this.init();
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.authService.userMenuEmit();
+  }
+  ngAfterViewInit() {}
+  // init() {
+  //   this.auth = new User();
+  //   this.auth.email = this.authService.getParamUserId();
+  // }
+  resetPassword(){
+    this.authService.resetPassword(this.auth).subscribe( (data: any) => {
+      this.authService.showMessage(data);
+      this.authService.navigateSignin();
+    })
+  }
 }
