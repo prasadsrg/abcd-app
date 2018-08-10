37492ee8a96511bb961dcffa242f92fb1fc76c93
Your Name <akhilbunny98@gmail.com>
2018-08-10 19:40:36 +0530

    changes in forgot password component and forgot password container

--- src/app/modules/auth/components/forgot-password/forgot-password.component.ts
+++ src/app/modules/auth/components/forgot-password/forgot-password.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Props } from "../../../../common/props";
 import { User } from "../../../../entities/user";
+import { ControlContainer, NgForm } from "@angular/forms";
+
 
 @Component({
   selector: "app-forgot-password",
   templateUrl: "./forgot-password.component.html",
-  styleUrls: ["./forgot-password.component.scss"]
+  styleUrls: ["./forgot-password.component.scss"],
+  viewProviders: [{ provide: ControlContainer, useExisting: NgForm }]
+  
 })
 export class ForgotPasswordComponent implements OnInit {
-  Props: any = Props;
   @Input() auth: User;
+  showorhide: string;
+  isVisible: string;
+  error: any;
+  Props: any = Props;
 
   constructor() {}
 
--- src/app/modules/auth/containers/forgot-password-container/forgot-password.container.ts
+++ src/app/modules/auth/containers/forgot-password-container/forgot-password.container.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit,Input} from "@angular/core";
 import { User } from "../../../../entities/user";
+import { Props } from "../../../../common/props";
+import { AuthService } from "../../common/auth.service";
+import { Storage } from "../../../../shared/utils/storage";
 
 @Component({
   selector: "app-forgot-password-container",
@@ -7,7 +10,23 @@ import { User } from "../../../../entities/user";
 })
 export class ForgotPasswordContainer implements OnInit {
   auth: User;
-  constructor() {}
+  Props: any = Props;
 
-  ngOnInit() {}
+  constructor(private authService: AuthService) {
+    Storage.clearSession();
+    this.auth = new User();
+  }
+
+  ngOnInit() {
+    this.authService.userMenuEmit();
+  }
+  ngAfterViewInit() {}
+
+  forgotpassword() {
+    this.authService.forgotPassword(this.auth).subscribe(data => {
+      this.authService.storageSave(data);
+      this.authService.navigateSignin();
+    });
+  }
+  
 }
