317f73d4e76a59119df08fe17be99683b61b6dc3
prasadsrg <prasad.srg@gmail.com>
2018-09-17 22:22:44 +0530

    commit

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -9,10 +9,14 @@ import { SharedModule } from "../../shared/shared.module";
 import { MaterialModule } from "../../material.module";
 import { ProfileTableComponent } from './components/profile-table/profile-table.component';
 import { ProfileFilterComponent } from './components/profile-filter/profile-filter.component';
+import { ProfileDetailsComponent } from './components/profile-details/profile-details.component';
+import { ProfileAddressComponent } from './components/profile-address/profile-address.component';
+import { ProfileActiveComponent } from './components/profile-active/profile-active.component';
+import { ProfileAdditionalComponent } from './components/profile-additional/profile-additional.component';
 
 @NgModule({
   imports: [CommonModule, ProfileRoutingModule, MaterialModule, SharedModule],
-  declarations: [ProfileSearchComponent, ProfileEditComponent, ProfileTableComponent, ProfileFilterComponent],
+  declarations: [ProfileSearchComponent, ProfileEditComponent, ProfileTableComponent, ProfileFilterComponent, ProfileDetailsComponent, ProfileAddressComponent, ProfileActiveComponent, ProfileAdditionalComponent],
   providers: [ProfileService]
 })
 export class ProfileModule {}
