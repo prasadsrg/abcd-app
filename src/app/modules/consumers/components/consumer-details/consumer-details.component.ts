23921bd44e6e6a302a64c8716aadea543c43d100
Dfftech <dfftech@localhost.localdomain>
2018-10-17 20:26:37 +0530

    commit

--- src/app/modules/consumers/components/consumer-details/consumer-details.component.ts
+++ src/app/modules/consumers/components/consumer-details/consumer-details.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ControlContainer, NgForm } from "@angular/forms";
+import { Props } from '../../../../common/props';
+import { Consumer } from '../../../../entities/consumer';
 
 @Component({
   selector: 'app-consumer-details',
   templateUrl: './consumer-details.component.html',
-  styleUrls: ['./consumer-details.component.scss']
+  styleUrls: ['./consumer-details.component.scss'],
+  viewProviders: [{ provide: ControlContainer, useExisting: NgForm }]
+
 })
 export class ConsumerDetailsComponent implements OnInit {
+  Props: any = Props;
 
   constructor() { }
+   
+  @Input()
+  consumer: Consumer;
 
   ngOnInit() {
   }
