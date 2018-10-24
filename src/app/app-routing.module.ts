7e28a40613e56a862f65feb406ddb5b464fd5b47
Dfftech <dfftech@localhost.localdomain>
2018-10-24 14:58:53 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,48 +7,7 @@ const routes: Routes = [
   {
     path: "welcome",
     loadChildren: "./modules/welcome/welcome.module#WelcomeModule"
-  },
-  {
-    path: "auth",
-    loadChildren: "./modules/auth/auth.module#AuthModule"
-  },
-  {
-    path: "dashboard",
-    loadChildren: "./modules/dashboard/dashboard.module#DashboardModule",
-    canActivate: [AuthService]
-  },
-  {
-    path: "profile",
-    loadChildren: "./modules/profile/profile.module#ProfileModule",
-    canActivate: [AuthService]
-  },
-  {
-    path: "access",
-    loadChildren: "./modules/access/access.module#AccessModule",
-    // canActivate: [AuthService]
-  },
-  {
-    path: "consumers",
-    loadChildren: "./modules/consumers/consumers.module#ConsumersModule",
-    canActivate: [AuthService]
-  },
-  {
-    path: "branches",
-    loadChildren: "./modules/branches/branches.module#BranchesModule",
-    canActivate: [AuthService]
-  },
-  {
-    path: "reports",
-    loadChildren: "./modules/reports/reports.module#ReportsModule",
-    canActivate: [AuthService]
-  },
-  {
-    path: "settings",
-    loadChildren: "./modules/settings/settings.module#SettingsModule",
-    // canActivate: [AuthService]
   }
-
-
 
 ];
 
